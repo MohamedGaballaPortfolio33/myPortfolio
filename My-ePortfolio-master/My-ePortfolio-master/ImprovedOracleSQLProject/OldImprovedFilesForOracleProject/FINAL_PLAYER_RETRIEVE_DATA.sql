6882f5047688a47c61bc94773658f623cb537281
MohamedGaballaPortfolio33 <56989728+MohamedGaballaPortfolio33@users.noreply.github.com>
2019-10-24 23:27:38 -0500

    Delete FINAL_PLAYER_RETRIEVE_DATA.sql

--- My-ePortfolio-master/My-ePortfolio-master/ImprovedOracleSQLProject/OldImprovedFilesForOracleProject/FINAL_PLAYER_RETRIEVE_DATA.sql
+++
@@ -1,6 +0,0 @@
-/*This will select and retrieve only the columns of firstname,lastname, and teamname players*/
-SELECT p.firstname, p.lastname, t.teamname
-FROM PLAYER_DATA p
-INNER JOIN 
-SPORTS_TEAM t
-ON(T.Teamid = p.Teamid);
