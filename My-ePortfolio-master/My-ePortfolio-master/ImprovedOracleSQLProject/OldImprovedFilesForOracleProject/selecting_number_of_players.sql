eb6eeed302b1907b9e68c0a6de7a4b0c958033fd
MohamedGaballaPortfolio33 <56989728+MohamedGaballaPortfolio33@users.noreply.github.com>
2019-10-24 23:29:02 -0500

    Delete selecting_number_of_players.sql

--- My-ePortfolio-master/My-ePortfolio-master/ImprovedOracleSQLProject/OldImprovedFilesForOracleProject/selecting_number_of_players.sql
+++
@@ -1,5 +0,0 @@
-/*This is to select the number of players from the PLAYER_DATA*/
-SELECT T.teamname, count(*) AS "number of players"
-FROM PLAYER_DATA P, SPORTS_TEAM T
-WHERE T.teamid = p.teamid
-GROUP BY T.teamname having  count(*)>2;
