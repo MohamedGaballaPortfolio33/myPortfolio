2f4af5257c7e949496681728c6e86288fd5ad5eb
MohamedGaballaPortfolio33 <56989728+MohamedGaballaPortfolio33@users.noreply.github.com>
2019-10-24 23:28:39 -0500

    Delete Using_INNER_JOINS.sql

--- My-ePortfolio-master/My-ePortfolio-master/ImprovedOracleSQLProject/OldImprovedFilesForOracleProject/Using_INNER_JOINS.sql
+++
@@ -1,7 +0,0 @@
-/* Selecting columns from the PLAYER_DATA using inner join*/
-SELECT p.firstname, p.lastname, t.city, t.teamname
-FROM PLAYER_DATA p
-INNER JOIN 
-SPORTS_TEAM t
-ON(T.Teamid = p.Teamid);
-
