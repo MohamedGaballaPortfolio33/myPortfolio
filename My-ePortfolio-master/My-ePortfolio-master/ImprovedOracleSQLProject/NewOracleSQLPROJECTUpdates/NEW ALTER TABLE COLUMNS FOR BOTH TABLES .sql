f1a41381498b02580ea838dc612b9fa7175cd57f
MohamedGaballaPortfolio33 <56989728+MohamedGaballaPortfolio33@users.noreply.github.com>
2019-10-24 23:25:45 -0500

    Delete NEW ALTER TABLE COLUMNS FOR BOTH TABLES .sql

--- My-ePortfolio-master/My-ePortfolio-master/ImprovedOracleSQLProject/NewOracleSQLPROJECTUpdates/NEW ALTER TABLE COLUMNS FOR BOTH TABLES .sql
+++
@@ -1,8 +0,0 @@
-/* Adding new columns to the tables for SPORTS_TEAM AND PLAYER_DATA*/
-ALTER TABLE SPORTS_TEAM
-ADD SPORTS_TYPE VARCHAR(15 char);
-
-ALTER TABLE Player_DATA
- ADD PlayersTelephone varchar(20);
- 
-
