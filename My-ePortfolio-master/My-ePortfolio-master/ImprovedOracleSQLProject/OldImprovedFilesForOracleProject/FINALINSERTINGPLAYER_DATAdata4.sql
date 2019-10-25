28befd4aa8fccbecbd7864831552c04bf6a7c277
MohamedGaballaPortfolio33 <56989728+MohamedGaballaPortfolio33@users.noreply.github.com>
2019-10-24 23:26:47 -0500

    Delete FINALINSERTINGPLAYER_DATAdata4.sql

--- My-ePortfolio-master/My-ePortfolio-master/ImprovedOracleSQLProject/OldImprovedFilesForOracleProject/FINALINSERTINGPLAYER_DATAdata4.sql
+++
@@ -1,4 +0,0 @@
-/* Inserting a new player into the PLAYER_DATA8*/
-INSERT INTO PLAYER_DATA
-(Playerid,firstname,lastname,Teamid,dateofbirth,social, TelePhone)
-VALUES (4,'John','Pomeraina',1,TO_DATE('04/25/1968', 'MM/DD/YYYY'),'128-09-6874', '(330)(234)(1213)');
